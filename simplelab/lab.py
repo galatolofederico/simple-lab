6ff3b129f9095cdabe5c0e3ab1c6de9d74309ab1
Federico Galatolo <galatolo.federico@gmail.com>
2020-02-02 17:38:20 +0100

    wrote simple argument parser

--- simplelab/lab.py
+++ simplelab/lab.py
@@ -1,6 +1,7 @@
 import yaml
 import sys
 import os
+import collections
 
 servers = None
 def loadservers():
@@ -12,6 +13,38 @@ def loadservers():
         print("File ~/.config/lab.servers.yml not found!")
         sys.exit(1)
 
+
+def getserver(name):
+    default = servers[0]
+    for server in servers:
+        if "default" in server and server["default"]:
+            default = server
+        if server["name"] == name:
+            return server, True
+    return default, False
+        
+
+
+def parsearguments():
+    help_str = """lab [server] action
+if [server] is omitted the default one is used
+action can be:
+
+* run <cmd>: Start experiment on [server] with command <cmd>
+* status: Check the status of all the servers
+* install: Install simplelab to server [server]
+* shared list: List all the experiment shared assets
+* shared clear <name>: Clear the content of shared assets <name>
+"""
+    server, shift = getserver(sys.argv[1])
+    args = sys.argv[1:]
+    if shift: args = args[1:]
+    Args = collections.namedtuple('Args', ['server', 'cmd', 'args'])
+
+    return Args(server, args[0], args[1:])
+
+
 def main():
     loadservers()
-    print("a", servers)
+    args = parsearguments()
+    print(args)
