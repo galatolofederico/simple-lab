5e688f46b70689cf51b85fcb213bb066aaca0a5f
Federico Galatolo <galatolo.federico@gmail.com>
2020-02-02 16:32:07 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simplelab",
+    version="0.1.0",
+    author="Federico A. Galatolo",
+    author_email="galatolo.federico@gmail.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=[
+
+    ],
+    entry_points = {
+        'console_scripts': ['lab=simplelab.lab:main'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Intended Audience :: Science/Research",
+        "Development Status :: 4 - Beta"
+    ],
+)
